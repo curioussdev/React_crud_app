e4ae492a86872bf31dfb9c3dbae3f3ff606b36be
José Lopes <946393361bill@gmail.com>
2024-01-22 00:20:46 +0000

    handleDelete DONE

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export function Header() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary" style={{background: "#000"}}>
             <Container>
-                <Navbar.Brand href="#home">Menu de Navegação</Navbar.Brand>
+                <Navbar.Brand href="#home">Projecto Crud</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -16,12 +16,12 @@ export function Header() {
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">
-                                Another action
+                               Outra Ação
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Alguma coisa</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">
-                                Separated link
+                                Link Separado
                             </NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { Button, Table } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link, useNavigate } from "react-router-dom";
 
 import userList from "../api/data";
 import Header from "./Header";
@@ -8,6 +9,19 @@ import Header from "./Header";
 
 
 export function Home() {
+
+    let history = useNavigate();
+
+    function handleDelte(e) {
+        var index = userList.map((e) => {
+            return e.id;
+        }).indexOf();
+
+        userList.splice(index, 1);
+
+        history('/')
+
+    }
 
     return(
         <Fragment>
@@ -20,6 +34,7 @@ export function Home() {
                         <th>Nome</th>
                         <th>Idade</th>
                         <th>Teste</th>
+                        <th>Ações</th>
                     </tr>
                 </thead>
 
@@ -35,6 +50,11 @@ export function Home() {
                                     <td>{item.name}</td>
                                     <td>{item.idade}</td>
                                     <td>Lúcia aqui não</td>
+                                    <td>
+                                        <Button onClick={() => alert(item.id)} variant="primary">Editar</Button>
+                                        &nbsp;
+                                        <Button onClick={()=> handleDelte(item.id)} variant="danger">Excluir</Button>
+                                    </td>
                                 </tr>
                             )
                         }) 
