fc69c8eeb0c410ea45da0d544ef79ae9030c0b8a
José Lopes <946393361bill@gmail.com>
2024-01-22 00:02:15 +0000

    navbar implemented

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from "react";
 
 import { Home } from "./components/Home";
 
+import { BrowserRouter as Router, Route, Routes,  } from "react-router-dom";
+
 const App = () => {
   return(
-    <Home />
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,34 +3,47 @@ import { Button, Table } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import userList from "../api/data";
+import Header from "./Header";
+
+
 
 export function Home() {
 
     return(
         <Fragment>
+            <Header  />
             <div style={{ margin: "10rem"}}>
+                <Table striped bordered hover>
                 <thead>
                     <tr>
                         <th>ID</th>
                         <th>Nome</th>
                         <th>Idade</th>
-                        <th></th>
+                        <th>Teste</th>
                     </tr>
                 </thead>
 
                 <tbody>
-                     {userList.map(user => (
-                        <tr key={user.id}>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.idade}</td>
-                            <td>
-                                <Button variant="primary" size="sm">Editar</Button>
-                                <Button variant="danger" size="sm">Excluir</Button>
-                            </td>
-                        </tr>
-                    ))}
+                    {
+                        
+                        userList && userList.length > 0 
+                        ? userList.map((item) => {
+                            return (
+                                <tr>
+                                    <td>{item.id}</td>
+                                    
+                                    <td>{item.name}</td>
+                                    <td>{item.idade}</td>
+                                    <td>Lúcia aqui não</td>
+                                </tr>
+                            )
+                        }) 
+                        :
+                        "Sem dados disponíveis"
+                    
+                    }
                 </tbody>
+                </Table>
             </div>
 
         </Fragment>
